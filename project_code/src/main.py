7b0b15ae465197cf6c77196c64828183f6c29d6f
ajm10565 <ajm10565@gmail.com>
2024-10-29 18:35:41 -0500

    changes

--- project_code/src/main.py
+++ project_code/src/main.py
@@ -1,9 +1,11 @@
 import json
 import sys
 import random
+from statistics import StatisticsError
 from typing import List
 from enum import Enum
 import turtle as t
+
 
 
 class EventStatus(Enum):
@@ -12,45 +14,85 @@ class EventStatus(Enum):
     FAIL = "fail"
     PARTIAL_PASS = "partial_pass"
 
-
-
 class Statistic:
-    def __init__(self, name: str, hero_class: str, health: int = 0, attack_power: int = 0):
+    def __init__(self, name: str,  value: int = 0):
         self.name = name
-        self.hero_class = hero_class
+        self.value = value
+
+
+class Character:
+    def __init__(self, name: str, health: int =100, attack_power = 1):
+        self.name = name
         self.health = health
-        self.attack_power = attack_power
+        self.attack_power =  attack_power
+        self.minimum_proc_chance = 70
+        self.minimum_damage = 1
         self.inventory = []
+        self.hero_class = "Hero"
+        if name == "Iron Man":
+            self.hero_class = "Genius"
+            self.strength = Statistic("Strength", 15)
+            self.intelligence = Statistic("Intelligence", 25)
+        elif name == "Captain America":
+            self.hero_class = "Super Soldier"
+            self.strength = Statistic("Strength",  20)
+            self.endurance = Statistic("Endurance",  15)
+        elif name == "Thor":
+            self.hero_class = "Asgardian"
+            self.strength = Statistic("Strength",  30)
+            self.magic = Statistic("Magic", 25)
+        else:
+            self.strength = Statistic("Strength",10)
+            self.intelligence = Statistic("Intelligence", 10)
 
     def __str__(self):
-        return f"{self.name} ({self.hero_class}) - HP: {self.health}, AP: {self.attack_power}"
+        return f"Character: {self.name}, Strength: {self.strength}, Intelligence: {self.intelligence}"
+
+    def get_stats(self):
+        if self.name == "Captain America":
+            return [self.strength, self.endurance]
+        elif self.name == "Thor":
+            return [self.strength, self.magic]
+        else:
+            return [self.strength, self.intelligence]
 
     def is_alive(self):
-        return self.health > 0
+        return self.health <= 0 # it should never be less than, but just in case
 
-    def attack(self, target: "Statistic"):
-        while True:
-            print(f"Do you want to use {self.name}'s special move? (y/n)")
-            use_special = input().strip().lower()
-            
-            # Check if input is valid
-            if use_special in {'y', 'yes'}:
-                self.special_move(target)
-                break
-            elif use_special in {'n', 'no'}:
-                success_chance = random.randint(1, 100)
-                if success_chance <= 70:
-                    damage = random.randint(1, self.attack_power)
-                    target.health -= damage
-                    print(f"{self.name} attacks {target.name} for {damage} damage!")
-                else:
-                    print(f"{self.name}'s attack missed!")
-                break
-            else:
-                print("Invalid choice. Please try again.")
+    def modify_health(self, amount):
+        new_health = self.health + amount
+        # remove
+        if new_health < 0:
+            self.health = 0
+        def add_item(self, item: str):
+            self.inventory.append(item)
+
+    def use_item(self, item: str):
+        if item in self.inventory:
+            print(f"{self.name} uses {item}.")
+            if item == "Vibranium Shield":
+                self.health += 30
+                print(f"{self.name} heals for 30 HP with Vibranium Shield!")
+            self.inventory.remove(item)
+
+    def attack(self, statistic, kind_of_attack, target):
+        if kind_of_attack == "Special":
+            self.special_move(target)
+        else:
+            self.basic_attack(statistic, target)
+
+    def basic_attack(self, statistic: Statistic, target: "Character"):
+        success_chance = random.randint(1, 100)
+        if success_chance <= self.minimum_proc_chance:
+            damage = random.randint(statistic.value, self.attack_power)
+            target.modify_health(-1*damage)
+            print(f"{self.name} attacks {target.name} for {damage} damage!")
+        else:
+            print(f"{self.name}'s attack missed!")
 
 
-    def special_move(self, target: "Statistic"):
+
+    def special_move(self, target: "Character"):
         if self.hero_class == "Genius":
             print(f"{self.name} uses Repulsor Blast!")
             success_chance = random.randint(1, 100)
@@ -68,69 +110,63 @@ class Statistic:
             self.health += 20
             print(f"{self.name} heals 20 HP with Shield Block!")
 
-    def add_item(self, item: str):
-        self.inventory.append(item)
 
-    def use_item(self, item: str):
-        if item in self.inventory:
-            print(f"{self.name} uses {item}.")
-            if item == "Vibranium Shield":
-                self.health += 30
-                print(f"{self.name} heals for 30 HP with Vibranium Shield!")
-            self.inventory.remove(item)
 
     def show_inventory(self):
         return self.inventory
 
 
-class Character:
-    def __init__(self, name: str):
-        self.name = name
-        if name == "Iron Man":
-            self.strength = Statistic("Strength", "Genius", health=100, attack_power=15)
-            self.intelligence = Statistic("Intelligence", "Genius", health=100, attack_power=25)
-        elif name == "Captain America":
-            self.strength = Statistic("Strength", "Super Soldier", health=120, attack_power=20)
-            self.endurance = Statistic("Endurance", "Super Soldier", health=120, attack_power=15)
-        elif name == "Thor":
-            self.strength = Statistic("Strength", "Asgardian", health=150, attack_power=30)
-            self.magic = Statistic("Magic", "Asgardian", health=150, attack_power=25)
-        else:
-            self.strength = Statistic("Strength", "Hero", health=100, attack_power=10)
-            self.intelligence = Statistic("Intelligence", "Hero", health=100, attack_power=10)
-
-    def __str__(self):
-        return f"Character: {self.name}, Strength: {self.strength}, Intelligence: {self.intelligence}"
-
-    def get_stats(self):
-        if self.name == "Captain America":
-            return [self.strength, self.endurance]
-        elif self.name == "Thor":
-            return [self.strength, self.magic]
-        else:
-            return [self.strength, self.intelligence]
-
-    def is_alive(self):
-        stats = self.get_stats()
-        return any(stat.is_alive() for stat in stats)
 
 
-class Enemy(Statistic):
-    def __init__(self, name: str, health: int, attack_power: int):
-        super().__init__(name, "Villain", health, attack_power)
 
-    def attack(self, target: "Statistic"):
-        success_chance = random.randint(1, 100)
-        if success_chance <= 60:
-            damage = random.randint(5, self.attack_power)
-            target.health -= damage
-            print(f"{self.name} attacks {target.name} for {damage} damage!")
-        else:
-            print(f"{self.name}'s attack missed!")
+class UserInputParser:
+    def parse(self, prompt: str) -> str:
+        return input(prompt)
+
+    def select_party_member(self, party: List[Character]) -> Character:
+        print("Choose a Marvel hero:")
+        for idx, member in enumerate(party):
+            print(f"{idx + 1}. {member.name}")
+
+        while True:
+            try:
+                choice = int(self.parse("Enter the number of the chosen hero: ")) - 1
+                if 0 <= choice < len(party):
+                    return party[choice]
+            except ValueError:
+                pass
+            print("Invalid choice. Please try again.")
+
+    def select_stat(self, character: Character) -> Statistic:
+        print(f"Select a stat for {character.name}:")
+        stats = character.get_stats()
+        for idx, stat in enumerate(stats):
+            print(f"{idx + 1}. {stat.name}")
+
+        while True:
+            try:
+                choice = int(self.parse("Enter the number of the chosen stat: ")) - 1
+                if 0 <= choice < len(stats):
+                    return stats[choice]
+            except ValueError:
+                pass
+            print("Invalid choice. Please try again.")
+
+    def what_kind_of_attack(self, character: Character)-> str:
+        while True:
+            print(f"Do you want to use {character.name}'s special move? (y/n)")
+            use_special = input().strip().lower()
+            # Check if input is valid
+            if use_special in {'y', 'yes'}:
+                return "Special"
+            elif use_special in {'n', 'no'}:
+                return "normal"
+
+
 
 
 class Event:
-    def __init__(self, data: dict, enemy: Enemy = None):
+    def __init__(self, data: dict, enemy: Character = None):
         self.primary_attribute = data['primary_attribute']
         self.secondary_attribute = data['secondary_attribute']
         self.prompt_text = data['prompt_text']
@@ -150,20 +186,23 @@ class Event:
             chosen_stat = parser.select_stat(character)
             self.resolve_choice(character, chosen_stat)
 
-    def battle_with_enemy(self, party: List[Character], parser):
+    def battle_with_enemy(self, party: List[Character], parser: UserInputParser):
         while self.enemy.is_alive() and any(member.is_alive() for member in party):
             for member in party:
                 if member.is_alive():
                     character = member
                     chosen_stat = parser.select_stat(character)  # Select stat to attack
-                    chosen_stat.attack(self.enemy)  # Attack using the chosen stat
+                    attack_kind = parser.what_kind_of_attack(character)
+                    character.attack(chosen_stat, attack_kind, self.enemy)
+
+                    # chosen_stat.attack(self.enemy)  # Attack using the chosen stat
 
                     if not self.enemy.is_alive():
                         print(f"{self.enemy.name} has been defeated!")
                         self.status = EventStatus.PASS
                         break
                 if self.enemy.is_alive():
-                    self.enemy.attack(chosen_stat)  # Enemy attacks the chosen stat
+                    self.enemy.attack(member)  # Enemy attacks the chosen stat
 
         if not any(member.is_alive() for member in party):
             print("Your party has been defeated!")
@@ -181,7 +220,6 @@ class Event:
             self.status = EventStatus.FAIL
             print(self.fail_message)
 
-
 class FinalBoss(Event):
     def __init__(self):
         super().__init__({
@@ -193,7 +231,7 @@ class FinalBoss(Event):
             'partial_pass': {'message': 'You wound Thanos but he escapes for now.'}
         })
         # Thanos has high health and attack power for a more intense battle
-        self.enemy = Enemy("Thanos", health=150, attack_power=35)
+        self.enemy = Character("Thanos", health=150, attack_power=35)
 
     def execute(self, party: List[Character], parser):
         print(self.prompt_text)
@@ -212,14 +250,12 @@ class FinalBoss(Event):
             self.status = EventStatus.PASS
             print(self.pass_message)
 
-
 class Location:
     def __init__(self, events: List[Event]):
         self.events = events
 
     def get_event(self) -> Event:
         return random.choice(self.events)
-
 
 class Game:
     def __init__(self, parser, characters: List[Character], locations: List[Location]):
@@ -259,39 +295,6 @@ class Game:
         return False
 
 
-class UserInputParser:
-    def parse(self, prompt: str) -> str:
-        return input(prompt)
-
-    def select_party_member(self, party: List[Character]) -> Character:
-        print("Choose a Marvel hero:")
-        for idx, member in enumerate(party):
-            print(f"{idx + 1}. {member.name}")
-    
-        while True:
-            try:
-                choice = int(self.parse("Enter the number of the chosen hero: ")) - 1
-                if 0 <= choice < len(party):
-                    return party[choice]
-            except ValueError:
-                pass
-            print("Invalid choice. Please try again.")
-
-    def select_stat(self, character: Character) -> Statistic:
-        print(f"Select a stat for {character.name}:")
-        stats = character.get_stats()
-        for idx, stat in enumerate(stats):
-            print(f"{idx + 1}. {stat.name}")
-        
-        while True:
-            try:
-                choice = int(self.parse("Enter the number of the chosen stat: ")) - 1
-                if 0 <= choice < len(stats):
-                    return stats[choice]
-            except ValueError:
-                pass
-            print("Invalid choice. Please try again.")
-
 
 
 
@@ -306,7 +309,7 @@ def main():
     ]
 
     # Setup enemies and events
-    enemy1 = Enemy("Loki", 50, 20)
+    enemy1 = Character("Loki", 50, 20)
     event1 = Event({
         'primary_attribute': 'Strength',
         'secondary_attribute': 'Magic',
@@ -316,7 +319,7 @@ def main():
         'partial_pass': {'message': 'You fought Loki, but he managed to escape!'}
     }, enemy=enemy1)
 
-    enemy2 = Enemy ("Ultron", 75, 25)
+    enemy2 = Character ("Ultron", 75, 25)
     event2 = Event({
     'primary_attribute': 'Intelligence',
     'secondary_attribute': 'Strength',
@@ -332,7 +335,6 @@ def main():
     # Setup game and start
     game = Game(parser, heroes, [location1, location2])
     game.start()
-
 
 if __name__ == "__main__":
     main()

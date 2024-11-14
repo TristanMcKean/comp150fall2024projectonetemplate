4ba650a90c904840f520648ef1f39efd25727c75
kyleanderson <kanderson24@luc.edu>
2024-11-13 22:30:24 -0600

    hopefully this works

--- project_code/src/main.py
+++ project_code/src/main.py
@@ -443,6 +443,10 @@ def main():
     game = Game(parser, heroes, [location1, location2])
     game.start()
 
+import os
 
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get("PORT", 5000))  # Render sets $PORT
+    app.run(host="0.0.0.0", port=port)
+
+
